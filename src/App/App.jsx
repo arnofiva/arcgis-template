a56c5ec6043fa983b3b312525c8f263e57387352
RalucaNicola <ralucagnicola@gmail.com>
2022-01-24 23:02:36 +0100

    Add ArcGIS API for JavaScript

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,31 @@
 import * as styles from './App.module.css';
 
+import { useEffect, useRef } from 'react';
+import MapView from "@arcgis/core/views/MapView";
+import WebMap from "@arcgis/core/WebMap";
+
 export function App() {
-  return <div className={styles.main}>Hello world</div>;
+
+  const mapDiv = useRef(null);
+
+  useEffect(() => {
+    if (mapDiv.current) {
+
+      const webmap = new WebMap({
+        portalItem: {
+          id: "aa1d3f80270146208328cf66d022e09c"
+        }
+      });
+
+      const view = new MapView({
+        container: mapDiv.current,
+        map: webmap
+      });
+
+    }
+  }, []);
+
+  return (<div className={styles.mapDiv} ref={mapDiv}></div>);
 }
+
+export default App;
