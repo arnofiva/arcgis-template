255de7e08a4d7f79044ed692287b791ac74cb16a
Raluca Nicola <RNicola@esri.com>
2022-01-25 09:27:13 +0100

    Add prettier and apply new styles

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,25 +1,21 @@
 import * as styles from './App.module.css';
 
 import { useEffect, useRef } from 'react';
-import SceneView from "@arcgis/core/views/SceneView";
-import WebScene from "@arcgis/core/WebScene";
+import SceneView from '@arcgis/core/views/SceneView';
+import WebScene from '@arcgis/core/WebScene';
 
-import "@esri/calcite-components/dist/components/calcite-card";
-import {
-  CalciteCard
-} from "@esri/calcite-components-react";
-import "@esri/calcite-components/dist/calcite/calcite.css";
+import '@esri/calcite-components/dist/components/calcite-card';
+import { CalciteCard } from '@esri/calcite-components-react';
+import '@esri/calcite-components/dist/calcite/calcite.css';
 
 export function App() {
-
   const mapDiv = useRef(null);
 
   useEffect(() => {
     if (mapDiv.current) {
-
       const webscene = new WebScene({
         portalItem: {
-          id: "1616213768fd4418905da6b1f3af9591"
+          id: '1616213768fd4418905da6b1f3af9591'
         }
       });
 
@@ -27,7 +23,6 @@ export function App() {
         container: mapDiv.current,
         map: webscene
       });
-
     }
   }, []);
 
@@ -35,14 +30,10 @@ export function App() {
     <>
       <div className={styles.mapDiv} ref={mapDiv}></div>
       <CalciteCard className={styles.card}>
-        <img
-          alt="footer thumbnail"
-          slot="thumbnail"
-          src="./assets/image.jpg"
-        />
-        <h3 slot="title">A beautiful hiking day</h3>
-        <span slot="subtitle">Seealpsee region, Switzerland</span>
-        <span slot="footer-leading">June 20, 2021</span>
+        <img alt='footer thumbnail' slot='thumbnail' src='./assets/image.jpg' />
+        <h3 slot='title'>A beautiful hiking day</h3>
+        <span slot='subtitle'>Seealpsee region, Switzerland</span>
+        <span slot='footer-leading'>June 20, 2021</span>
       </CalciteCard>
     </>
   );
